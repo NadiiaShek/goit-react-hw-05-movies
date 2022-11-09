2187922f30a26aa648a75740176103dd8f16e3f1
Nadiia <nadiia.shek@gmail.com>
2022-11-09 20:39:56 +0200

    Update theme.jsx

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -3,11 +3,11 @@ export const theme = Object.freeze({
     white: '#ffffff',
     active: '#0F4C75',
     grey: '#7b7580',
-    greyBg: '#BBE1FA',
+    greyBg: '#7ebae2',
     greyOpacity: 'rgba(47, 48, 58, 0.1)',
     primaryText: '#1f0d4d',
     secondaryText: '#000000',
-    buttonBg: '#3282B8',
+    buttonBg: '#7ebae2',
     boxShadow:
       '0px 1px 3px rgba(0, 0, 0, 0.22), 0px 2px 2px rgba(0, 0, 0, 0.24), 0px 2px 1px rgba(0, 0, 0, 0.32)',
   },
